6c48ad102a136198306b2036e4e018fef7c14bb8
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-12-28 02:56:42 +0100

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your code here
+class Breakfast{
+    food;
+    drink;
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    };
+};
+
+class Lunch{
+    salad;
+    soup;
+    drink;
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    };
+};
+
+class Dinner{
+    soup;
+    entree;
+    salad;
+    #desert;
+    constructor(salad, soup, entree, desert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#desert = desert;
+    }
+}
+
